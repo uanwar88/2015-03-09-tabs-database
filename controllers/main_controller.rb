282c2ed505c9d047df9a977c74062265b618d09c
Usman Anwar <uanwar88@gmail.com>
2015-03-09 16:13:01 -0500

    Working with hash location

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -1,11 +1,15 @@
-get "/product/:id" do
-  id = params[:id].to_i
-  @product = Product.find(id)
-  slim :product
-end
+# get "/product/:id" do
+#   id = params[:id].to_i
+#   @product = Product.find(id)
+#   slim :product
+# end
 
 get "/product" do
-  id = params[:id].to_i
+  if params[:id]
+    id = params[:id].to_i
+  else
+    id = 1
+  end
   @product = Product.find(id)
   slim :product
 end
--- public/global.js
+++ public/global.js
@@ -5,6 +5,10 @@ window.onload = function() {
   // console.log(nav_links.length);
   // console.log(tab_contents.length);
 
+  // Show initial tab on page load
+  var hash_location = window.location.hash.substring(1) || "general_info";
+  var initial_tab = document.getElementById(hash_location);
+  initial_tab.style.display = "block";
 
   // for (var link in nav_links) {
   //   console.log("Added a listener!");
@@ -13,6 +17,7 @@ window.onload = function() {
   //   };
   // }
 
+  // Show tab on link click
   for (var i = 0; i < nav_links.length; i++) {
     nav_links[i].onclick = function () {
       for (var x = 0; x < tab_contents.length; x++) {
